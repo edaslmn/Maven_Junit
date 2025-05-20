6172ab872332ec25901625442db350caaff543cd
edaslmn <eda--slmn@hotmail.com>
2025-05-21 02:55:25 +0300

    Güncel dosyalar eklendi

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,13 +1,15 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class ReusableMethods {
 
-    public static void bekle(int saniye){
+    public static void bekle(int saniye)  {
 
         try {
             Thread.sleep(saniye*1000);
@@ -36,4 +38,37 @@ public class ReusableMethods {
 
     }
 
+    public static void urlIleWindowGecisi(WebDriver driver, String hedefUrl){
+
+        Set<String> acikOlanTumWindowlarinWhdSeti = driver.getWindowHandles();
+
+        for (String eachWhd :acikOlanTumWindowlarinWhdSeti){
+
+            driver.switchTo().window(eachWhd);
+
+            String actualUrl = driver.getCurrentUrl();
+
+            if (hedefUrl.equals(actualUrl)){
+                break;
+            }
+
+        }
+    }
+
+
+
+    public static void titleIleWindowGecisi(WebDriver driver, String hedefTitle){
+        Set<String> acikOlanTumWindowlarinWhdSeti = driver.getWindowHandles();
+        for (String eachWhd :acikOlanTumWindowlarinWhdSeti){
+            driver.switchTo().window(eachWhd);
+            String actualTitle = driver.getTitle();
+            if (hedefTitle.equals(actualTitle)){
+                break;
+            }
+        }
+    }
+
+
 }
+
+
