d5ade3a404844413f51024416f69fda4443c7306
edaslmn <eda--slmn@hotmail.com>
2025-05-28 22:50:41 +0300

    ScreenShot

---
+++ src/test/java/tests/day17_excelAutomation_getScreenShot/C02_WriteExcel.java
@@ -0,0 +1,24 @@
+package tests.day17_excelAutomation_getScreenShot;
+
+import org.junit.jupiter.api.Test;
+
+public class C02_WriteExcel {
+
+    @Test
+    public void test01(){
+
+        //1) Yeni bir Class olusturalim WriteExcel
+        //2) Yeni bir test method olusturalim writeExcelTest()
+        //3) Adimlari takip ederek Sayfa1’e kadar gidelim
+        //4) 1.satir 5.hucreye yeni bir cell olusturalim
+        //5) Olusturdugumuz hucreye "Nufus" yazdiralim
+        //6) 2.satir nufus kolonuna 1500000 yazdiralim
+        //7) 10.satir nufus kolonuna 250000 yazdiralim
+        //8) 15.satir nufus kolonuna 54000 yazdiralim
+        //9) Dosyayi kaydedelim
+        //10)Dosyayi kapatalim
+
+
+
+    }
+}
---
+++ src/test/java/tests/day17_excelAutomation_getScreenShot/C04_FaildOlanTestScreenshot.java
@@ -0,0 +1,4 @@
+package tests.day17_excelAutomation_getScreenShot;
+
+public class C04_FaildOlanTestScreenshot {
+}
